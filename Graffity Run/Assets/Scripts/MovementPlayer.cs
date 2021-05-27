fb3c4d62ef05375418fe12c79714f8f18001835c
HF <f2hafner@edu.aau.at>
2021-05-27 18:42:03 +0200

    Added Gravity script section in MovementPlayer.cs

--- Graffity Run/Assets/Scripts/MovementPlayer.cs
+++ Graffity Run/Assets/Scripts/MovementPlayer.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class MovementPlayer : MonoBehaviour
 {
+    public GameObject playercam;
     public GameObject player;
-    public float speed = 10;
+    public float speed = 10F;
+    public bool gravityReverse = false; //true means gravity reversed
+    public bool onGround = false; // is player on a Ground
+    public float gravity = 1F;
+    
 
     void Update()
     {
-        player.transform.position += new Vector3(speed*Time.deltaTime,0,0);
+        // Keypress [Gravity Change]
+        if(onGround == true){
+            if(Input.GetKeyDown("space")){
+                player.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            }
+        }
+
+        // Movement
+        playercam.transform.position += new Vector3(speed*Time.deltaTime,0,0);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        onGround = true;
+    }
+    void OnCollisionExit2D(Collision2D col) {
+        onGround = false;
     }
 }
